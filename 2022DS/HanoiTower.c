61c46bd41e343b9d648eadae5e04415b60ebdb9b
juwan-25 <s2109@e-mirim.hs.kr>
2022-03-26 18:21:27 +0900

    HanoiTower실습

---
+++ 2022DS/HanoiTower.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+int factorial(int n) {
+	if (n == 1) return 1;
+	return n * factorial(n - 1);
+}
+
+int main(void) {
+	int n = 5;
+	printf("%d! = %d\n", n, factorial(n));
+	return 0;
+}
